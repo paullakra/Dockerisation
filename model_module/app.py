0be0db49232c57899bdd88b559d35555f5b3f978
paullakra <75439618+paullakra@users.noreply.github.com>
2022-03-24 11:26:18 +0530

    Minor changes

--- model_module/app.py
+++ model_module/app.py
@@ -1,15 +1,36 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 import uvicorn
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
-from model_module import generate_summary
+from transformers import BertTokenizerFast, EncoderDecoderModel
+import torch
 
 
 class TextInput(BaseModel):
-    unix_time: int
     text: str
-    user_id: int
+
+
+def load_model():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tokenizer = BertTokenizerFast.from_pretrained(
+        'mrm8488/bert-small2bert-small-finetuned-cnn_daily_mail-summarization')
+    model = EncoderDecoderModel.from_pretrained(
+        'mrm8488/bert-small2bert-small-finetuned-cnn_daily_mail-summarization').to(
+        device)
+    return tokenizer, model, device
+
+
+def generate_summary(text):
+    # cut off at BERT max length 512
+    tokenizer, model, device = load_model()
+    inputs = tokenizer([text], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = inputs.input_ids.to(device)
+    attention_mask = inputs.attention_mask.to(device)
+
+    output = model.generate(input_ids, attention_mask=attention_mask)
+
+    return tokenizer.decode(output[0], skip_special_tokens=True)
 
 
 app = FastAPI()
@@ -22,10 +43,11 @@ def root():
 
 @app.post("/input", response_class=PlainTextResponse)
 def get_summary(text_wrapper: TextInput):
-    # send inference to database instead of user
+    # TODO: send inference to database instead of user
     return generate_summary(text_wrapper.text)
 
 
 if __name__ == "__main__":
     # Run uvicorn server from inside docker
     uvicorn.run(app, host="0.0.0.0", port=5000)
+    # uvicorn.run(app)
